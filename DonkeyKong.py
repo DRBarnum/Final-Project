9c1e664f2c2b42a71512593a351aad7c0befa27a
DRBarnum <douglasbarnum@hanovernorwichschools.org>
2016-11-22 13:55:52 -0500

    Updated from Brython Server: 11/22/2016 1:55:54 PM

--- DonkeyKong.py
+++ DonkeyKong.py
@@ -1,1 +1,32 @@
+import ggame
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, CircleAsset
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+# Barrel
+class Barrel(Sprite):
+    brown = Color(0x996633, 1.0)
+    Black = Color(1,0)
+    yellow = Color(0xfdd835, 1.0)
+    noline = LineStyle(1, Black)
+    asset = CircleAsset(50, noline, brown)
+    rect = RectangleAsset(50, 50, noline, yellow)
+    
+    
+
+    
+# THE WORLD
+class Kong(App):
+    
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black=Color(1, 0)
+        Black=Color(0, 1)
+        noline=LineStyle(1000, Black)
+        bg_asset=RectangleAsset(width, height, noline, black)
+        bg=Sprite(bg_asset, (0, 0))
+    
+
+myapp = Kong(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
