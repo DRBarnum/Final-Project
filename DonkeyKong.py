866818cc57233fa8511fecdab71f65c90d73ff9c
DRBarnum <douglasbarnum@hanovernorwichschools.org>
2016-11-30 13:15:32 -0500

    Updated from Brython Server: 11/30/2016 1:15:32 PM

--- DonkeyKong.py
+++ DonkeyKong.py
@@ -31,7 +31,11 @@ class Kong(App):
         noline=LineStyle(1000, Black)
         bg_asset=RectangleAsset(width, height, noline, black)
         bg=Sprite(bg_asset, (0, 0))
-        Barrel((100,100))
+        Wall((0,0))
+        wall((0,0))
+        wAll((0,0))
+        waLl((0,0))
+        
         
 
             
@@ -40,7 +44,8 @@ class Kong(App):
 class Wall(Sprite):
     Red = Color(0xF44366, 1.0)
     noline = LineStyle(0, Red)
-    asset = PolygonAsset([(0, 700), (0, 600), (700, 400), (700, 600)], noline, Red)
+    asset = PolygonAsset([(0, 670), (0, 700), (700, 680), (700, 650)], noline, Red)
+    
     
     def __init__(self, position):
         super().__init__(Wall.asset, position)
@@ -48,11 +53,52 @@ class Wall(Sprite):
         self.vy = 0
         self.vr = 0
         self.fxcenter = self.fycenter = 0
-       
-
+        
     
         
+class wall(Sprite):
+    Red = Color(0xF44366, 1.0)
+    noline = LineStyle(0, Red) 
+    Asset = PolygonAsset([(0, 480), (0, 510), (600, 580), (600, 550)], noline, Red) 
 
+    
+    def __init__(self, position):
+        super().__init__(wall.Asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 0
+        self.fxcenter = self.fycenter = 0
+        
+class wAll(Sprite):
+    Red = Color(0xF44366, 1.0)
+    noline = LineStyle(0, Red) 
+    Asset = PolygonAsset([(100, 380), (100, 350), (700, 260), (700, 290)], noline, Red) 
+
+    
+    def __init__(self, position):
+        super().__init__(wAll.Asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 0
+        self.fxcenter = self.fycenter = 0
+        
+class waLl(Sprite):
+    Red = Color(0xF44366, 1.0)
+    noline = LineStyle(0, Red) 
+    Asset = PolygonAsset([(0, 150), (0, 180), (600, 180), (600, 150)], noline, Red) 
+
+    
+    def __init__(self, position):
+        super().__init__(waLl.Asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 0
+        self.fxcenter = self.fycenter = 0
+
+class ladder(Sprite):
+    Blue = Color(0x4fc3f7)
+    noline = LineyStyle(0, Blue)
+    
     
             
 
